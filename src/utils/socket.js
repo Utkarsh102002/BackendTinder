f7534e1d0e5fdca9094aa981a9d54df9dafd0395
Utkarsh Shrivastava <utkarshshrivastava102002@gmail.com>
2025-11-26 00:45:17 +0530

    socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
-const { Chat } = require("../models/chat");
+const Chat = require("../models/chat.js");
 const ConnectionRequest = require("../models/connectionRequest");
 
 const getSecretRoomId = (userId, targetUserId) => {
